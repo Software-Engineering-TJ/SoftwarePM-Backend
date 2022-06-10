0f38b652a3d5f968035a3a9b42f130f095cfec41
HJK <1654364091@qq.com>
2022-06-10 11:22:13 +0800

    debug

--- src/main/java/com/tongji/software_management/entity/DBEntity/Attend.java
+++ src/main/java/com/tongji/software_management/entity/DBEntity/Attend.java
@@ -16,12 +16,6 @@ public class Attend {
     private String courseId;
     private String classId;
     private String title;
-    private int year;
-    private int month;
-    private int day;
-    private int hour;
-    private int minute;
-    private int percent;
     private String endTime;
     private String startTime;
 
@@ -56,66 +50,6 @@ public class Attend {
     }
 
     @Basic
-    @Column(name = "year")
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    @Basic
-    @Column(name = "month")
-    public int getMonth() {
-        return month;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    @Basic
-    @Column(name = "day")
-    public int getDay() {
-        return day;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
-    }
-
-    @Basic
-    @Column(name = "hour")
-    public int getHour() {
-        return hour;
-    }
-
-    public void setHour(int hour) {
-        this.hour = hour;
-    }
-
-    @Basic
-    @Column(name = "minute")
-    public int getMinute() {
-        return minute;
-    }
-
-    public void setMinute(int minute) {
-        this.minute = minute;
-    }
-
-    @Basic
-    @Column(name = "percent")
-    public int getPercent() {
-        return percent;
-    }
-
-    public void setPercent(int percent) {
-        this.percent = percent;
-    }
-
-    @Basic
     @Column(name = "end_time")
     public String getEndTime() {
         return endTime;
@@ -140,11 +74,11 @@ public class Attend {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Attend attend = (Attend) o;
-        return year == attend.year && month == attend.month && day == attend.day && hour == attend.hour && minute == attend.minute && percent == attend.percent && Objects.equals(courseId, attend.courseId) && Objects.equals(classId, attend.classId) && Objects.equals(title, attend.title) && Objects.equals(endTime, attend.endTime) && Objects.equals(startTime, attend.startTime);
+        return Objects.equals(courseId, attend.courseId) && Objects.equals(classId, attend.classId) && Objects.equals(title, attend.title) && Objects.equals(endTime, attend.endTime) && Objects.equals(startTime, attend.startTime);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(courseId, classId, title, year, month, day, hour, minute, percent, endTime, startTime);
+        return Objects.hash(courseId, classId, title, endTime, startTime);
     }
 }
